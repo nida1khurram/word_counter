a1db3b0fcea3ca80f524e2862aefc7aa7370d802
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-07-13 18:30:17 +0500

    colorful cli

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 
+console.log(chalk.yellow("**********************************************"));
+console.log(chalk.green("WORD COUNTER PROJECT"));
+
+console.log(chalk.yellow("**********************************************"));
 let ans = await inquirer.prompt(
     [{
         name:"text",
         type:"input",
-        message:"Please Enter Your Text",
+        message:chalk.blue("Please Enter Your Text"),
     }]
 );
 const text:string=ans.text.trim().split(" ");
-// console.log(`${text}`);
-console.log(`Words in text : ${text.length}`);
+console.log(chalk.magenta("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"));
 
+console.log(chalk.red`Total Words in your  text : ${chalk.green.underline(text.length)}`);
+
+console.log(chalk.cyan("Thank you "));
+
+console.log(chalk.magenta("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"));
